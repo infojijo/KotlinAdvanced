eb67bc158d145679f9730accb16e6c8b30a008c7
Jijo Jose <info.jijo@gmail.com>
2024-06-27 08:23:02 -0400

    added circular and linear switching progressbar

--- app/src/main/java/com/example/myapp1/views/MainActivity.kt
+++ app/src/main/java/com/example/myapp1/views/MainActivity.kt
@@ -8,13 +8,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +27,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,9 +40,7 @@ import com.example.myapp1.views.theme.MyApp1Theme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
     private val listViewModel by viewModels<ListDataViewModel>()
-
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,26 +52,24 @@ class MainActivity : ComponentActivity() {
                 var showProgress by remember { mutableStateOf(true) }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.White
+                    color = Color.White,
                 ) {
                     LazyColumn(
                         state = listState,
-                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                        verticalArrangement = Arrangement.spacedBy(10.dp),
                     ) {
                         stickyHeader {
                             Text(
-                                text = "Hey, Comments updated below!!"
+                                text = "Hey, Comments updated below!!",
                             )
                         }
                         item {
-                            if (showProgress) {
-                                ShowProgress()
-                            }
+                            ShowProgress(showProgress, false)
                         }
                         lifecycleScope.launch {
                             listViewModel.getFlowResult()
                             listViewModel.flowList.observe(
-                                this@MainActivity
+                                this@MainActivity,
                             ) { commentList ->
                                 when (commentList.status) {
                                     DataStatus.Status.LOADING -> {}
@@ -77,27 +78,28 @@ class MainActivity : ComponentActivity() {
                                         for (comment in commentList.data!!) {
                                             item {
                                                 Box(
-                                                    modifier = Modifier
-                                                        .padding(10.dp)
+                                                    modifier =
+                                                    Modifier
+                                                        .padding(10.dp),
                                                 ) {
                                                     Text(
                                                         text = comment.id.toString() + " - " + comment.email,
                                                         fontSize = 18.sp,
-                                                        color = Color.Black.copy(alpha = 0.5f)
+                                                        color = Color.Black.copy(alpha = 0.5f),
                                                     )
                                                 }
                                                 Box(
-                                                    modifier = Modifier
-                                                        .padding(10.dp)
+                                                    modifier =
+                                                    Modifier
+                                                        .padding(10.dp),
                                                 ) {
                                                     Text(
                                                         text = comment.body,
                                                         fontSize = 14.sp,
-                                                        color = Color.Black.copy(alpha = 0.5f)
+                                                        color = Color.Black.copy(alpha = 0.5f),
                                                     )
                                                 }
                                             }
-
                                         }
                                         coroutineScope.launch { listState.scrollToItem(0) }
                                         item {
@@ -123,22 +125,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
-        text = "Hello $name!", modifier = modifier
+        text = "Hello $name!",
+        modifier = modifier,
     )
 }
 
 @Composable
-fun ShowProgress() {
-    Box(modifier = Modifier.fillMaxSize()) {
+fun ShowProgress(loading: Boolean, isCircleType: Boolean) {
+    if (!loading) return
+    Column(
+        modifier =
+        Modifier
+            .padding(48.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {if(isCircleType){
+        CircularProgressIndicator(
+            modifier = Modifier.width(45.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )}
+        else{
         LinearProgressIndicator(
-            modifier = Modifier
+            modifier =
+            Modifier
                 .fillMaxSize()
                 .height(18.dp)
                 .background(Color.LightGray),
-            color = Color.Green
+            color = Color.Green,
         )
+    }
     }
 }
 
