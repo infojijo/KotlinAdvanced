9570a92f47132eee675c711c8071e63452c5b454
Jijo Jose <info.jijo@gmail.com>
2024-07-03 23:41:03 -0400

    All and Any example

--- app/src/main/java/com/example/myapp1/views/NextActivity.kt
+++ app/src/main/java/com/example/myapp1/views/NextActivity.kt
@@ -69,5 +69,6 @@ fun main() {
             listOfPersons.filter { it.age > 4 }.apply { println("Size->${this.size}") }
         }"
     )
-
+    println("All are above 4 ? :  ${listOfPersons.all { it.age > 4 }}")
+    println("Anyone older than 4? :  ${listOfPersons.any { it.age > 4 }}")
 }
