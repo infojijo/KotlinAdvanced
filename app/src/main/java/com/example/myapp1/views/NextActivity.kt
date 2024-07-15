a202ffc0aa0dd129a0b361c1a4eacea152df9c55
Jijo Jose <info.jijo@gmail.com>
2024-07-15 12:21:53 -0400

    Design Principles

--- app/src/main/java/com/example/myapp1/views/NextActivity.kt
+++ app/src/main/java/com/example/myapp1/views/NextActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
