fa01afcd9d61c7e8b3fda41cbe60a29784cbb236
Jijo Jose <info.jijo@gmail.com>
2024-07-15 22:29:16 -0400

    Design Principles - Before O/C principle

--- app/src/main/java/com/example/myapp1/arch/SOLID.kt
+++ app/src/main/java/com/example/myapp1/arch/SOLID.kt
@@ -1,5 +1,44 @@
 package com.example.myapp1.arch
 
-fun main(){
+fun main() {
     println("Welcome to SOLID World!!")
+    val dr = Car()
+    print("${dr.make} - ${dr.yom} - is ").also { dr.d() }
+}
+
+open class Drive {
+    fun d() {
+        println("driving -> in DRIVE mode")
+    }
+
+    fun r() {
+        println("driving -> in REVERSE mode")
+    }
+
+    fun n() {
+        println("driving -> in NEUTRAL mode")
+    }
+}
+
+
+class Car() : Drive() {
+    var make: String = "Honda"
+    var yom: Int = 2016
+}
+
+interface DriveMode {
+    fun drive()
+}
+
+open class Forward : DriveMode {
+    override fun drive() {
+        println("from->${javaClass.name}")
+    }
+
+}
+
+open class Reverse : DriveMode {
+    override fun drive() {
+        println("from->${javaClass.name}")
+    }
 }
